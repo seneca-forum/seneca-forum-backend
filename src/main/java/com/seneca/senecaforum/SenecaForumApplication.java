ec24d4af0ae1a14e2880e034a302f51aee07f4e6
viphavethcm <loivang.nhatduy@gmail.com>
2021-04-24 14:13:33 +0700

    first commit

---
+++ src/main/java/com/seneca/senecaforum/SenecaForumApplication.java
@@ -0,0 +1,13 @@
+package com.seneca.senecaforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenecaForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SenecaForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seneca/senecaforum/SenecaForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seneca.senecaforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenecaForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
