9ffb37724bcae58975dfa557e7ade0ebfbd0acd3
trangntt-016 <trangntt.016@gmail.com>
2021-04-29 12:17:11 -0400

    edit TopicController testRepositoryTopic

--- src/main/java/com/seneca/senecaforum/client/controller/TopicController.java
+++ src/main/java/com/seneca/senecaforum/client/controller/TopicController.java
@@ -1,9 +1,31 @@
 package com.seneca.senecaforum.client.controller;
 
+import com.seneca.senecaforum.domain.Topic;
+import com.seneca.senecaforum.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.HttpHeaders;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class TopicController {
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @GetMapping("/topics")
+    public ResponseEntity<List<Topic>> getAllTopics() {
+        List<Topic> topics = topicRepository.findAll();
+        if(topics.size()==0){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(topics);
+    }
+
+
 }
