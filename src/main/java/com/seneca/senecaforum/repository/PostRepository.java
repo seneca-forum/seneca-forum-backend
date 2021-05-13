b589d00e015b01b8ce48e7e9451b505aad7ffb83
viphavethcm <loivang.nhatduy@gmail.com>
2021-05-13 10:42:20 +0700

    update no of comments

--- src/main/java/com/seneca/senecaforum/repository/PostRepository.java
+++ src/main/java/com/seneca/senecaforum/repository/PostRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.Temporal;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
