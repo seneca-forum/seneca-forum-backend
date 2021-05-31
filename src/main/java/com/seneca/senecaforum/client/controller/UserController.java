f1a6c2f1cf84295ae38a60ee4d22913fe7922c57
trangntt-016 <trangntt.016@gmail.com>
2021-05-31 18:28:47 -0400

    config security, services, payload, CustomUserDetails, CustomUserDetailsService

--- src/main/java/com/seneca/senecaforum/client/controller/UserController.java
+++ src/main/java/com/seneca/senecaforum/client/controller/UserController.java
@@ -9,7 +9,14 @@ import com.seneca.senecaforum.service.UserService;
 import com.seneca.senecaforum.service.dto.UserDto;
 import com.seneca.senecaforum.service.utils.MapperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -23,27 +30,54 @@ public class UserController {
     @Autowired
     private RoleService roleService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
     @PostMapping("/new")
-    public ResponseEntity<UserDto> createNewUser(@RequestBody User usr){
+    public ResponseEntity<UserDto> createNewUser(@RequestBody User usr) {
         Role role = roleService.getRoleByRoleName(usr.getRole().getRoleName());
-        if(role == null){
-            throw new BadRequestException("Rolename "+usr.getRole().getRoleName()+"does not exist!");
+        if (role == null) {
+            throw new BadRequestException("Rolename " + usr.getRole().getRoleName() + "does not exist!");
         }
-        if(!userService.isUsernameUnique(usr.getUsername())){
-            throw new BadRequestException("Username "+usr.getUsername()+" already exists!");
+        if (!userService.isUsernameUnique(usr.getUsername())) {
+            throw new BadRequestException("Username " + usr.getUsername() + " already exists!");
         }
-        if(!userService.isEmailUnique(usr.getEmail())){
-            throw new BadRequestException("Email "+usr.getEmail()+" already exists!");
+        if (!userService.isEmailUnique(usr.getEmail())) {
+            throw new BadRequestException("Email " + usr.getEmail() + " already exists!");
         }
         User user = null;
-        try{
+        try {
             usr.setRole(role);
             user = userService.saveUser(usr);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             throw new InternalException("Cannot save this user to the database");
         }
-        UserDto savedUsr = MapperUtils.mapperObject(user,UserDto.class);
+        UserDto savedUsr = MapperUtils.mapperObject(user, UserDto.class);
         return ResponseEntity.ok(savedUsr);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDto> login(@RequestBody User usr) {
+        Authentication authentication = null;
+        if(userService.isEmailUnique(usr.getEmail())){
+           throw new BadRequestException("Email "+usr.getEmail()+" is not found!");
+        }
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(usr.getEmail(), usr.getPassword())
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BadCredentialsException("Email and Password is not matched!");
+        }
+        // update is rememberMe
+        User savedUsr = userService.updateIsRememberMe(usr);
+        String jwt = userService.createNewToken(authentication,usr.getIsRememberMe());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Authorization", "Bearer " + jwt);
+
+        UserDto userDto = MapperUtils.mapperObject(savedUsr,UserDto.class);
+        return new ResponseEntity<>(userDto, httpHeaders, HttpStatus.OK);
+
     }
 }
---
+++ src/main/java/com/seneca/senecaforum/security/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.seneca.senecaforum.security;
+
+import com.seneca.senecaforum.security.jwt.JwtFilter;
+import com.seneca.senecaforum.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("customUserDetailsService")
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(getPasswordEncoder());
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.antMatcher("/api/users/login")
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+}
